95f4caefc1a23a0af5da94decc765f78c61a85bf
MaximKlaiman <maximklaiman@gmail.com>
2025-12-16 18:25:30 -0800

    hash h

--- hash.h
+++ hash.h
@@ -5,6 +5,8 @@
 #include <cmath>
 #include <random>
 #include <chrono>
+#include <string>
+#include <cctype>
 
 typedef std::size_t HASH_INDEX_T;
 
@@ -19,16 +21,47 @@ struct MyStringHash {
     // hash function entry point (i.e. this is h(k))
     HASH_INDEX_T operator()(const std::string& k) const
     {
-        // Add your code here
+        unsigned long long w[5] = {0ULL, 0ULL, 0ULL, 0ULL, 0ULL};
 
+        int n = (int)k.size();
+        int wi = 4;
 
+        for(int end = n; end > 0 && wi >= 0; end -= 6)
+        {
+            int start = end - 6;
+            if(start < 0) start = 0;
+
+            unsigned long long val = 0ULL;
+
+            // base-36 conversion (avoid pow): val = val*36 + digit
+            for(int j = start; j < end; ++j)
+            {
+                char c = (char)std::tolower((unsigned char)k[j]);
+                val = val * 36ULL + (unsigned long long)letterDigitToNumber(c);
+            }
+
+            w[wi] = val;   // w[4] is last chunk, then w[3], ...
+            wi--;
+        }
+
+        unsigned long long h = 0ULL;
+        for(int i = 0; i < 5; ++i)
+        {
+            h += w[i] * (unsigned long long)rValues[i];
+        }
+        return (HASH_INDEX_T)h;
     }
 
     // A likely helper function is to convert a-z,0-9 to an integral value 0-35
     HASH_INDEX_T letterDigitToNumber(char letter) const
     {
-        // Add code here or delete this helper function if you do not want it
-
+        if(letter >= 'a' && letter <= 'z') {
+            return (HASH_INDEX_T)(letter - 'a');
+        }
+        if(letter >= '0' && letter <= '9') {
+            return (HASH_INDEX_T)(26 + (letter - '0'));
+        }
+        return 0;
     }
 
     // Code to generate the random R values
